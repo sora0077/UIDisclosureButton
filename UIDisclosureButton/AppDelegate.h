211784b6e92a8394f135c364ebb826e5c2a299ef
t.hayashi <t.hayashi0077+github@gmail.com>
2012-03-29 23:48:14 +0900

    Initial Commit

---
+++ UIDisclosureButton/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIDisclosureButton
+//
+//  Created by 達也 林 on 12/03/29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
